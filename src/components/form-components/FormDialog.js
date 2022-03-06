6416cbb6425724a1160003305fc4a5f325f69638
kevin <karvin_jimenez@hotmail.com>
2022-03-06 18:18:34 -0400

    Implementanda la nueva libreria react-hook-form para el manejo de formularios. Funciona correctamente y ya con esto reemplace la forma basica que utilizo para trabajar los formularios

--- src/components/form-components/FormDialog.js
+++ src/components/form-components/FormDialog.js
@@ -1,25 +1,22 @@
 import React, {useEffect, useState} from "react";
-import {Dialog, DialogActions, DialogContent, DialogTitle,} from "@mui/material";
+import {Dialog, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Select, TextField,} from "@mui/material";
 import {getAllVideoGamesGenre, saveVideoGame, updateVideoGame} from "../../services/VideoGameService";
 //Este es el modulo a importar para utilizar los proptypes, esto la primera vez debe ser agregado al package-json
 import PropTypes from 'prop-types';
 import Button from "@mui/material/Button";
-import GeneralTextField from "./GeneralTextField";
-import GeneralSelect from "./GeneralSelect";
+import {useForm} from "react-hook-form";
 
 export default function FormDialog(props) {
 
     //obtengo todos los elementos que utilizare de mi props mediante Destructuring
     const {actualVideoGame, handleClose, isDialogOpen, setVideoGames} = props;
 
+    //Comento getValues ya que por ahora no lo utilizo, solo lo defini para entendimiento de su uso
+    const {register, handleSubmit, setValue, /*getValues,*/} = useForm();
+
     //En mi componente lo ideal es solo definir estados y variables que seran utilizados solos en este mismo, hay excepciones claro
     const [id, setId] = useState(0);
     const [name, setName] = useState("");
-    const [developer, setDeveloper] = useState("");
-    const [genre, setGenre] = useState("RPG");
-    const [gameModes, setGameModes] = useState("");
-    const [sellPrice, setSellPrice] = useState(0);
-    const [stock, setStock] = useState(0);
 
     //la forma mas facil de tener el mismo dialog para crear y editar es simplemente pasarle los datos del actualgame
     //a los estados correspondientes, siempre y cuando el juego actual este presente
@@ -28,31 +25,29 @@ export default function FormDialog(props) {
         //verifico que el videojuego actual este para asi llenar los estados
         if(actualVideoGame){
 
+            setValue("id", actualVideoGame.id)
+            setValue("name", actualVideoGame.name);
+            setValue("developer", actualVideoGame.developer);
+            setValue("genre", actualVideoGame.genre);
+            setValue("gameModes", actualVideoGame.gameModes);
+            setValue("sellPrice", actualVideoGame.sellPrice);
+            setValue("rating", actualVideoGame.rating);
+            setValue("stock", actualVideoGame.stock);
+
             setId(actualVideoGame.id);
             setName(actualVideoGame.name);
-            setDeveloper(actualVideoGame.developer);
-            setGenre(actualVideoGame.genre);
-            setGameModes(actualVideoGame.gameModes);
-            setSellPrice(actualVideoGame.sellPrice);
-            setStock(actualVideoGame.stock);
         }
 
         //el useEffect se ejecutara cada vez el videojuego actual cambie por lo tanto siempre tendre el juego correcto a la hora de editar
-    }, [actualVideoGame]);
-
-    //creada funcion general para el manejo de cambio de datos en los formularios
-    const generalHandleChange = (setData, event) => {
-
-        setData(event.target.value);
-    };
+    }, [actualVideoGame, setValue]);
 
 
-    const handleSubmit = () => {
+    const onSubmit = (data) => {
 
-        const videoGameToSave = {id, name, developer, genre, gameModes, sellPrice, stock};
+        const videoGameToSave = data;
 
         //si el id actual esta disponible que actualice el videogame y sino que solo cree uno nuevo
-        if (id)
+        if (data.id)
             updateVideoGame(videoGameToSave, setVideoGames);
 
         else
@@ -64,13 +59,19 @@ export default function FormDialog(props) {
 
     const resetFormData = () => {
 
-        setId(0);
-        setName("");
-        setDeveloper("");
-        setGenre("RPG");
-        setGameModes("");
-        setSellPrice(0);
-        setStock(0);
+        //Mediante getValues podremos obtener todos los valores del form mediante un objeto
+        // const {name, developer, genre, stock, sellPrice, rating} = getValues();
+
+        //Mediante setValue podemos llenar los campos del formulario mediante codigo,
+        // perfecto para cuando se quier limpiar los valores del formulario o para cuando vayamos a editar
+        //Primero indicamos el nombre del campo y luego el valor que tendra el campo
+        setValue("name", "");
+        setValue("developer", "");
+        setValue("genre", "");
+        setValue("gameModes", "");
+        setValue("sellPrice", "");
+        setValue("rating", "");
+        setValue("stock", "");
 
         handleClose();
     };
@@ -96,43 +97,64 @@ export default function FormDialog(props) {
 
                 <DialogContent>
 
-                    <GeneralTextField value={name} handleChange={generalHandleChange}
-                                      label={"Name"} type={"text"} setData={setName}/>
+                    {/*"handleSubmit" will validate your inputs before invoking "onSubmit" */}
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        {/* register your input into the hook by invoking the "register" function */}
 
-                    <GeneralTextField value={developer} handleChange={generalHandleChange}
-                                      label={"Developer"} type={"text"} setData={setDeveloper}/>
+                        {/*Para hacer uso de react hook-form debemos de agregar el campo de register, seguido del
+                            nombre que tendra nuestro campo, esto sera lo que se evaluara a la hora de presionar el boton submit*/}
+                        <TextField{...register("name")} autoFocus margin="dense" label="Nombre" type="text" fullWidth/>
 
-                    <GeneralTextField value={gameModes} handleChange={generalHandleChange}
-                                      label={"Game Modes"} type={"text"} setData={setGameModes}/>
+                        <TextField{...register("developer")} autoFocus margin="dense"
+                                  label="Developer" type="text" fullWidth/>
 
-                    <GeneralTextField value={sellPrice} handleChange={generalHandleChange}
-                                      label={"Selling Price"} type={"number"} setData={setSellPrice}/>
+                        <TextField {...register("gameModes")} autoFocus margin="dense"
+                                   label="Game Modes" type="text" fullWidth/>
 
-                    <GeneralTextField value={stock} handleChange={generalHandleChange}
-                                      label={"Stock"} type={"number"} setData={setStock}/>
+                        <TextField {...register("rating")} autoFocus margin="dense"
+                                   label="Rating" type="number" fullWidth/>
 
-                    <GeneralSelect value={genre} handleChange={generalHandleChange}
-                                   label={"Genre"} setData={setGenre} dataList={getAllVideoGamesGenre()}/>
+                        <TextField {...register("sellPrice")} autoFocus margin="dense"
+                                  label="Selling Price" type="number" fullWidth/>
+
+                        <TextField {...register("stock")} autoFocus margin="dense"
+                                   label="Stock" type="number" fullWidth/>
+
+
+                        <FormControl fullWidth>
+
+                            <InputLabel id="demo-simple-select-label">Genero</InputLabel>
+
+                            <Select
+                                {...register("genre")}
+                                defaultValue={"RPG"}
+                            >
+                                {getAllVideoGamesGenre().map((data, index) => (
+
+                                    <MenuItem key={index} value={data}>{data}</MenuItem>
+                                ))}
+                            </Select>
+
+                        </FormControl>
+
+                        {id === 0 ? (
+                                <Button variant="contained" color="primary" type="submit">
+                                    Add Video Game
+                                </Button>
+                            ):
+
+                            <Button variant="contained" color="primary" type="submit">
+                                Update Video Game
+                            </Button>
+                        }
+
+                        <Button variant="contained" color="secondary" onClick={handleCancel}>
+                            Cancel
+                        </Button>
+
+                    </form>
 
                 </DialogContent>
-
-                <DialogActions>
-
-                    {id === 0 ? (
-                        <Button variant="contained" color="primary" onClick={handleSubmit}>
-                            Add Video Game
-                        </Button>
-                    ):
-
-                        <Button variant="contained" color="primary" onClick={handleSubmit}>
-                            Update Video Game
-                        </Button>
-                    }
-
-                    <Button variant="contained" color="secondary" onClick={handleCancel}>
-                        Cancel
-                    </Button>
-                </DialogActions>
 
             </Dialog>
         </div>
